99344e4d0fc4ea412aafc86b319fab704ad81cd4
viper3400 <viper_3400@gmx.de>
2017-08-09 19:10:58 +0200

    Implemented simple command binding as example

--- src/Jaxx.MUI.Template/ViewModel/View2ViewModel.cs
+++ src/Jaxx.MUI.Template/ViewModel/View2ViewModel.cs
@@ -1,26 +1,51 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Jaxx.MUI.Template.ViewModel
 {
-    public class View2ViewModel
+    public class View2ViewModel : ViewModelBase
 
     {
+        private string _info;
 
         public View2ViewModel()
 
         {
 
             Info = "This is View 2";
+            ButtonClicked = new RelayCommand<EventArgs>(ExecuteButtonClicked, CanExecuteButtonClicked);
 
         }
 
+        private bool CanExecuteButtonClicked(EventArgs arg)
+        {
+            return _info != "ExecuteButtonClicked";
+        }
 
+        private void ExecuteButtonClicked(EventArgs obj)
+        {
+            Info = "ExecuteButtonClicked";
+        }
+        
 
-        public string Info { get; set; }
+        public ICommand ButtonClicked { get; private set; }
+        public string Info
+        {
+            get { return _info; }
+            set
+            {
+                if (_info == value)
+                    return;
+                _info = value;
+                RaisePropertyChanged();
+            }
+        }
 
     }
 }
